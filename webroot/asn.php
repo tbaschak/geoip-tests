4c5d523afcce365346463c06f1ce495d34055e0f
Theo Baschak <theodore@ciscodude.net>
2014-06-02 23:58:54 -0500

    add beginning ASN info page

---
+++ webroot/asn.php
@@ -0,0 +1,31 @@
+<?php
+
+include("../inc/config.inc.php");
+include("../inc/hcdn.geoip.php");
+include("../lib/geoip-api-php/src/geoip.inc");
+
+$ip = $_SERVER["REMOTE_ADDR"];
+$ipver = ipVersion($ip);
+
+if($ipver == 4)
+{
+  $giasn = geoip_open($GEOIP_PATH . "/GeoIPASNum.dat", GEOIP_STANDARD);
+  $asn = geoip_name_by_addr($giasn, $ip);
+
+} elseif($ipver == 6)
+{
+  $giasn = geoip_open($GEOIP_PATH . "/GeoIPASNumv6.dat", GEOIP_STANDARD);
+  $asn = geoip_name_by_addr_v6($giasn, $ip);
+
+} else
+{
+  // shouldn't get here, error out
+  print "error, no IP";
+  exit();
+}
+
+print "$ip has asn " . $asn . "\n";
+
+geoip_close($giasn);
+
+?>
